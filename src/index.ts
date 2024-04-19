a8ee5f66df6714850ee46859d3704194defdd585
shaheenj <jawadishaheen@gmail.com>
2024-04-19 05:21:18 +0100

    Add CORS middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express  from 'express';
 import http from 'http';
+import cors from 'cors';
 import { Server } from 'socket.io';
 
 
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server);
 
