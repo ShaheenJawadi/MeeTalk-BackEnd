9026f1675cb46af8b42102fd4ccd9ab6c7c9ac71
shaheenj <jawadishaheen@gmail.com>
2024-04-19 05:11:46 +0100

    Add socket.io dependency and implement basic socket connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express  from 'express';
 import http from 'http';
+import { Server } from 'socket.io';
+
+
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('new connection');
+});
 
 const listen = () => server.listen(3500, () => console.log(`Server listening on port 3500`));
  
