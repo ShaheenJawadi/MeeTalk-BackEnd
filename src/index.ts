dc905d56fb17f6256cb3d40f30374131737e191e
shaheenj <jawadishaheen@gmail.com>
2024-04-19 05:46:21 +0100

    Update socket.io initialization to include CORS configuration and add a simple socket event handling

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,16 @@ import { Server } from 'socket.io';
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+});
 
 io.on('connection', (socket) => {
     console.log('new connection');
+    socket.on("send_msg", (data) => {
+        console.log(data); 
+    });
+    
 });
 
 const listen = () => server.listen(3500, () => console.log(`Server listening on port 3500`));
